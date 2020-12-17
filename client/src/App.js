89302cb31bf2bd95f6182d286deafa26c55cf92a
Remenems <cazinremi@gmail.com>
2020-12-17 23:08:15 +0100

    Premier affichage

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Join from './components/Joins';
-import Join from './components/chat';
+import Join from './components/Join';
+import Chat from './components/Chat';
 
 const App = () => (
     <Router>
